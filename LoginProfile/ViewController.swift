39e3cbd178148c37c9e945ca2a127f6040127c43
inju2403 <56947879+inju2403@users.noreply.github.com>
2022-01-03 17:18:18 +0900

    feat: (cheshire) Action Event 생성 이메일 또는 전화번호 입력 시 Action Event 생성

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -8,11 +8,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+
+    @IBAction func changedInput(_ sender: UITextField) {
+        print("test")
+        let inputLength = sender.text?.count
+        print(inputLength)
+        if let inputLength = inputLength, inputLength >= 5 {
+            print("input changed")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
