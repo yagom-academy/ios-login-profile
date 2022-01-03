dd851d1f8f1bbf639edc1a5a8b049b1dbb2d4f65
cheshire.kwon <cheshire.kwon@kakaocorp.com>
2022-01-03 17:43:15 +0900

    feat: (joel) underline 추가
    textfield에 underline 추가

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
 
     @IBAction func changedInput(_ sender: UITextField) {
         print("test")
@@ -19,10 +27,20 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private func setUI() {
+        idTextField.underlined()
+        pwTextField.underlined()
     }
-    
 }
 
+extension UITextField {
+    func underlined() {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.placeholderText.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: self.frame.size.height)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+}
