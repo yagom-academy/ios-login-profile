f6bec00f9db3a58867571db2b33cd785df39b257
claire-pp <claire.pp@kakaocorp.com>
2022-01-04 00:10:12 +0900

    feat: [추가] 버튼 색상 변경 기능
    이메일 또는 전화번호에 5글자 이상 입력시 로그인 버튼 색상 변경

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -7,12 +7,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailOrPhoneNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.emailOrPhoneNumber.addTarget(self, action: #selector(emailTextFieldDidChange(_:)), for: .editingChanged)
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func emailTextFieldDidChange(_ sender: Any?) {
+        guard let text = emailOrPhoneNumber.text else {
+            return
+        }
+        if text.count < 5 {
+            loginButton.backgroundColor = .systemGray5
+            return
+        }
+        loginButton.backgroundColor = .systemYellow
+    }
 }
 
