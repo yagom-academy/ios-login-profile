f0436797d10a49fa0bb44ffc6c4e5ceca54397ee
hayden.chae <cms7371@gmail.com>
2022-01-04 09:55:09 +0900

    feat: Step 1 제목, 설명 레이블 추가
    - 제목, 설명 레이블 추가

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -8,11 +8,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var instructionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    private func configureInstructionLabel() {
+        instructionLabel.numberOfLines = 2
+        instructionLabel.text = "사용하던 카카오계정이 있다면\n이메일 또는 전화번호로 로그인해 주세요"
+    }
 
 }
 
