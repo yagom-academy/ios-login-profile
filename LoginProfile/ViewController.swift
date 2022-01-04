172071f623fff7cc39a2ebfe1d68fc0f6338bfd1
Jinwook Huh <gjwlsdnr0115@gmail.com>
2022-01-04 11:00:47 +0900

    feat: Step1 loginButton 활성화 기능 구현
    - loginButton 활성화 기능 구현

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -6,12 +6,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailOrNumberTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setDelegate()
     }
+    
+    private func setDelegate() {
+        emailOrNumberTextField.delegate = self
+    }
+    
+    private func judgeEnableButton() {
+        if emailOrNumberTextField.text?.count ?? 0 >= 5 {
+            loginButton.backgroundColor = UIColor(named: "KakaoYellow")
+            loginButton.isEnabled = true
+            return
+        }
+        loginButton.backgroundColor = UIColor.systemGray6
+        loginButton.isEnabled = false
+    }
+   
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if textField == emailOrNumberTextField {
+            judgeEnableButton()
+        }
+    }
 }
 
