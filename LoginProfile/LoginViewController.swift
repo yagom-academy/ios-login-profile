1238a1317066e98ce561e6c5bae63c59928f89ba
HanEun-ji <ejhan97@naver.com>
2022-01-05 14:49:40 +0900

    로그인, 프로필 화면 [STEP 1] claire.pp, vapor.park (#2)
    * env: [툴] swift lint 추가
    
    컨벤션 준수를 위한 swift lint 추가
    
    * feat: [UI] UI 구현
    
    스토리보드 이용 로그인 화면 UI 구현
    
    * fix: [UI] text field placeholder 수정
    
    텍스트가 아닌 placeholder로 수정
    
    * feat: [추가] 버튼 색상 변경 기능
    
    이메일 또는 전화번호에 5글자 이상 입력시 로그인 버튼 색상 변경
    
    * chore: [수정] emailTextFieldDidChange 코드 수정
    
    코드 간결하게 수정
    
    * feat: [추가] 버튼 활성화/비활성화 기능
    
    5자를 입력하지 않으면 버튼이 비활성화되도록 구현
    
    * fix: [UI] textField 텍스트 크기 대응
    
    textField만 글자 크기가 변하지 않던 부분 수정
    
    * feat: [추가] 지역화 구현(en, ko)
    
    한국어와 영어에 지역화 구현
    
    * feat: [UI] 글자 크기 dynamic type 설정
    
    UI의 텍스트들 automatically adjusts font 체크
    
    * chore: [수정] 파일명, 코드 수정
    
    Main->Login, ViewController -> LoginViewController
    조금 더 각 파일이 무엇을 하는 파일인지를 명확히 함
    코드를 더 간결하게 수정함
    
    * chore: [수정] 변수 접근 private로 수정
    
    클래스 내부 IBOutlet 변수에 외부에서 접근할 수 없도록 private로 수정
    
    * fix: [UI] body 폰트 변경
    
    subtitle -> body
    figma 디자인 가이드에 있는 사항으로 수정
    
    * chore: [수정] 주석 제거
    
    가독성을 위해 주석 제거함
    
    * chore: [수정] parameter명 수정
    
    bool -> isActive
    조금 더 의미를 명확하게 수정함
    
    * fix: [UI] figma 가이드에 따라 수정
    
    figma 가이드에 맞지 않는 요소들 수정
    
    * fix: [UI] 색상 수정
    
    figma 가이드에 맞게 색상 수정
    
    Co-authored-by: claire-pp <claire.pp@kakaocorp.com>

---
+++ LoginProfile/LoginViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var emailOrPhoneNumber: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.emailOrPhoneNumber.addTarget(self, action: #selector(emailTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc
+    func emailTextFieldDidChange(_ sender: UITextField?) {
+        guard let text = sender?.text, text.count >= 5 else {
+            activateButton(false)
+            return
+        }
+        activateButton(true)
+    }
+    
+    func activateButton(_ isActive: Bool) {
+        loginButton.backgroundColor = isActive ? .systemYellow : .systemGray5
+        self.loginButton.isEnabled = isActive
+    }
+}
+
