fd2c90b35ec1d6e175e8e3895a216effd0c6aa7a
claire-pp <claire.pp@kakaocorp.com>
2022-01-05 14:39:23 +0900

    feat: [추가] 프로필 UI, viewcontroller 추가
    프로필 화면을 띄울 스토리보드와 뷰컨트롤러 추가

--- LoginProfile/LoginViewController.swift
+++ LoginProfile/LoginViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet private weak var loginButton: UIButton!
+
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet private weak var emailOrPhoneNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +16,15 @@ class LoginViewController: UIViewController {
             return
         }
         activateButton(true)
+    }
+    
+    @IBAction func onLoginButtonPressed(_ sender: Any) {
+        print("login")
+        let board = UIStoryboard(name: "Profile", bundle: nil)
+        let profileViewController = board.instantiateViewController(withIdentifier: "ProfileViewController")
+        profileViewController.modalPresentationStyle = .fullScreen
+        present(profileViewController, animated: true, completion: nil)
+        
     }
     
     func activateButton(_ isActive: Bool) {
---
+++ LoginProfile/ProfileViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+//        super.viewDidLoad()
+        let view = UIView()
+        view.backgroundColor = .yellow
+    }
+}
